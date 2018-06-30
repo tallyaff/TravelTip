112f82e28e584da61110dd1f806f2932781010c3
tally <tallyaffek@gmail.com>
2018-06-30 11:27:37 +0300

    'weather'

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -6,7 +6,7 @@ const W_KEY = '47115ca4e049c56eeef6b684e924cd20';
 
 function getWeather(lat, lng) {
 
-    var prm = axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
+    var prm = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
         .then(function (res) {
             console.log(res.data)
             renderWeather(res.data);
@@ -17,7 +17,7 @@ function getWeather(lat, lng) {
 function renderWeather(data){
  document.querySelector('.weather').innerHTML =
 `Wheater Today <br>
-<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/><br>
+<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"/><br>
   temperature from ${data.main.temp_min} to ${data.main.temp_max}, wind ${data.wind.speed}` ;
 }
 
